003f9ff2ec848075e79f2a59a288e14874814692
nstk24 <nastkinsergej24@gmail.com>
2024-12-23 17:22:06 +0400

    yo

--- app.py
+++ app.py
@@ -4,18 +4,14 @@ import os
 import pywt
 import random
 import flask
-import tkinter as tk
-from tkinter import filedialog
-from tkinter import messagebox
 import matplotlib.pyplot as plt
 from scipy.sparse.linalg import spsolve
 from scipy.signal import savgol_filter
 from scipy import sparse
-from tkinter import *
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 from matplotlib.figure import Figure
 from time import perf_counter
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 
 from scipy.signal import find_peaks
@@ -238,8 +234,12 @@ def process_and_plot():
     try:
         # Получение данных из POST-запроса
         data = request.json
+        print("Полученные данные:", data)  # Отладочный вывод
+
         frequencies_list = [np.array(freq) for freq in data['frequencies_list']]  # Частоты
         amplitudes_list = [np.array(ampl) for ampl in data['amplitudes_list']]    # Амплитуды
+        print("Частоты:", frequencies_list)
+        print("Амплитуды:", amplitudes_list)
 
         # Флаги обработки
         selection_flag = data.get('selection_flag', False)
@@ -249,10 +249,25 @@ def process_and_plot():
         normalize_flag = data.get('normalize_flag', False)
         average_flag = data.get('average_flag', False)
 
+        # Проверка параметров
+        print("Флаги:", {
+            "selection_flag": selection_flag,
+            "savgol_filter_flag": savgol_filter_flag,
+            "remove_flag": remove_flag,
+            "normalize_snv_flag": normalize_snv_flag,
+            "normalize_flag": normalize_flag,
+            "average_flag": average_flag
+        })
+
         # Параметры для каждой операции
         selection_params = data.get('selection_params', {})
         savgol_params = data.get('savgol_params', {})
         baseline_params = data.get('baseline_params', {})
+        print("Параметры:", {
+            "selection_params": selection_params,
+            "savgol_params": savgol_params,
+            "baseline_params": baseline_params
+        })
 
         # Применение операций в зависимости от флагов
         if selection_flag:
@@ -281,6 +296,10 @@ def process_and_plot():
         ax.set_xlabel('Рамановский сдвиг, см^-1')
         ax.set_ylabel('Интенсивность')
 
+        if normalize_flag:
+            amplitudes_list = normalize_by_max(amplitudes_list)
+
+
         # Сохранение графика в буфер
         buf = io.BytesIO()
         plt.savefig(buf, format='png')
@@ -292,7 +311,9 @@ def process_and_plot():
         # Возвращаем результат
         return jsonify({'plot_image': encoded_image})
     except Exception as e:
+        print("Ошибка:", str(e))  # Вывод ошибки в консоль
         return jsonify({'error': str(e)}), 400
+
 
 
 # Вспомогательные функции для обработки
@@ -323,34 +344,55 @@ def delete_baseline(amplitudes_list, params):
     return [
         amplitudes - baseline_als(amplitudes, lam, p) for amplitudes in amplitudes_list
     ]
+def normalize_by_max(spectrum_list):
+    normalized_spectrum = []
+    for spectrum in spectrum_list:
+        max_value = np.max(spectrum)
+        if max_value == 0:
+            raise ValueError("Максимум равен 0, нормализация невозможна.")
+        normalized_spectrum.append((spectrum / max_value).tolist())
+    return normalized_spectrum
+
 
 
 
 import os
 from flask import request
-
 @app.route('/upload_files', methods=['POST'])
 def upload_files():
     try:
-        # Проверяем, что файлы загружены
         if 'files' not in request.files:
             return jsonify({'error': 'Файлы не загружены'}), 400
 
-        files = request.files.getlist('files')  # Получаем список файлов
+        files = request.files.getlist('files')
         frequencies_list = []
         amplitudes_list = []
 
         for file in files:
-            # Читаем данные из файла
-            data = np.genfromtxt(file, skip_header=1)
-            frequencies_list.append(data[:, 0])
-            amplitudes_list.append(data[:, 1])
-         # Возвращаем данные для использования
+            # Прочитаем содержимое файла
+            content = file.read().decode('utf-8')
+            print("Содержимое файла:\n", content)
+
+            # Обработаем данные
+            try:
+                # Заменяем запятые на точки для корректной работы с числами
+                content = content.replace(',', '.')
+
+                # Используем табуляцию как разделитель
+                data = np.genfromtxt(io.StringIO(content), delimiter='\t')
+
+                frequencies_list.append(data[:, 0])  # Первый столбец - частоты
+                amplitudes_list.append(data[:, 1])  # Второй столбец - амплитуды
+            except Exception as e:
+                print("Ошибка обработки файла:", str(e))
+                return jsonify({'error': f'Ошибка обработки файла: {file.filename}', 'details': str(e)}), 400
+
         return jsonify({
             'frequencies_list': [freq.tolist() for freq in frequencies_list],
             'amplitudes_list': [ampl.tolist() for ampl in amplitudes_list]
         })
     except Exception as e:
+        print("Общая ошибка:", str(e))
         return jsonify({'error': str(e)}), 400
 
 @app.route('/set_flags', methods=['POST'])
@@ -392,233 +434,10 @@ def clear_data():
     except Exception as e:
         return jsonify({'error': str(e)}), 400
 
-# Создание окна
-root = tk.Tk()
-root.title("Spectrum")
-root.state('zoomed')
-
-# Устанавливаем размеры и положение окна на полный экран
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-root.attributes("-fullscreen", True)
-root.geometry(f"{screen_width}x{screen_height}+0+0")
-
-def show_help():
-    message = "Эта программа предназначена для обработки и визуализации спектральных данных.\n\n" \
-              "Использование программы:\n\n" \
-              "1. Выберите файлы с данными, нажав кнопку 'Файл' -> 'Открыть файлы'.\n" \
-              "   Поддерживаемые форматы файлов: ESP, TXT.\n\n" \
-              "2. После открытия файлов, программа автоматически построит график спектров.\n\n" \
-              "3. Для дальнейшей обработки спектров, воспользуйтесь меню 'Действия':\n\n" \
-              "   - Выбор частот и амплитуд в заданном диапазоне: позволяет выбрать диапазон частот и амплитуд, которые будут отображаться на графике.\n\n" \
-              "   - Сглаживание: применяет фильтр Савицкого-Голоя для сглаживания спектров.\n\n" \
-              "   - Удаление базовой линии: удаляет базовую линию из спектров методом ALS (Alternating Least Squares).\n\n" \
-              "   - Нормализация: нормализует спектры методом стандартного отклонения или по максимальному значению.\n\n" \
-              "   - Вычисление средней спектрограммы: вычисляет среднюю спектрограмму по всем спектрам.\n\n" \
-              "4. После выбора необходимых действий, нажмите кнопку 'run', программа построит обновленный график на основе выбранных действий и данных из выбранных файлов.\n\n" \
-              "5. Для завершения работы программы, нажмите кнопку 'Файл' -> 'Выход'."
-    messagebox.showinfo("Помощь", message)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
-def show_programm_info():
-    message = "О программе\n\n" \
-              "Имя: Программа для обработки спектров\n" \
-              "Эта программа предназначена для обработки и визуализации спектров.\n" \
-              "Программа позволяет выбирать файлы с данными, выполнять различные действия над спектрами и визуализировать результаты.\n" \
-              "В меню 'Действия' доступны следующие функции:\n" \
-              "- Выбор частот и амплитуд в заданном диапазоне\n" \
-              "- Сглаживание\n" \
-              "- Удаление базовой линии\n" \
-              "- Нормализация\n" \
-              "- Вычисление средней спектрограммы\n\n" \
-    
-    messagebox.showinfo("О программе", message)
-
-
-
-# Панель инструментов
-mainmenu = tk.Menu(root)
-root.config(menu=mainmenu)
-# Открытие файла с
-filemenu = tk.Menu(mainmenu, tearoff=0)
-filemenu.add_command(label="Открыть файлы", command=open_folder)
-filemenu.add_command(label="Сохранить...")
-filemenu.add_separator()
-filemenu.add_command(label="Выход", command=lambda: root.destroy())
-# Помощь
-helpmenu = tk.Menu(mainmenu, tearoff=0)
-helpmenu.add_command(label="Помощь", command = show_help)
-helpmenu.add_command(label="О программе", command= show_programm_info)
-
-building = tk.Menu(mainmenu, tearoff=0)
-building.add_command(label="Построить график", command=get_input)
-building.add_command(label="Очистить данные о файле", command=clear_data)
-# Поле ввода 1
-
-# Создаем метку (Label) с текстом
-label = tk.Label(root, text="lam:", font=("Arial", 10))
-label.pack()  # Размещаем метку на окне
-label.place(x=5, y=140)
-
-# Создаем метку (Label) с текстом
-label = tk.Label(root, text="p:", font=("Arial", 10))
-label.pack()  # Размещаем метку на окне
-label.place(x=5, y=160)
-
-# Создаем метку (Label) с текстом
-label = tk.Label(root, text="min:", font=("Arial", 10))
-label.pack()  # Размещаем метку на окне
-label.place(x=5, y=50)
-
-# Создаем метку (Label) с текстом
-label = tk.Label(root, text="max:", font=("Arial", 10))
-label.pack()  # Размещаем метку на окне
-label.place(x=5, y=70)
-
-# Создаем метку (Label) с текстом
-label = tk.Label(root, text="wlen:", font=("Arial", 10))
-label.pack()  # Размещаем метку на окне
-label.place(x=5, y=283)
-
-# Создаем метку (Label) с текстом
-label = tk.Label(root, text="poly:", font=("Arial", 10))
-label.pack()  # Размещаем метку на окне
-label.place(x=5, y=303)
-
-label = tk.Label(root, text="width:", font=("Arial", 10))
-label.pack()  # Размещаем метку на окне
-label.place(x=5, y=385)
-
-label = tk.Label(root, text="prom:", font=("Arial", 10))
-label.pack()  # Размещаем метку на окне
-label.place(x=5, y=405)
-# Полоса частотного диапазона
-# Поле ввода 1 (min_freq)
-entry1 = tk.Entry(root)
-entry1.insert(0, "min_freq")
-entry1.bind("<FocusIn>", on_entry_click)
-entry1.pack()
-entry1.place(x=40, y=50)
-# Поле ввода 2 (max_freq)
-entry2 = tk.Entry(root)
-entry2.insert(0, "max_freq")
-entry2.bind("<FocusIn>", on_entry_click)
-entry2.pack()
-entry2.place(x=40, y=70)
-# baseline_correction
-# Поле ввода 3 (lam)
-entry3 = tk.Entry(root)
-entry3.insert(0, "1000")
-entry3.bind("<FocusIn>", on_entry_click)
-entry3.pack()
-entry3.place(x=40, y=142)
-# Поле ввода 4 (p)
-entry4 = tk.Entry(root)
-entry4.insert(0, "0.001")
-entry4.bind("<FocusIn>", on_entry_click)
-entry4.pack()
-entry4.place(x=40, y=162)
-# savgol_filter
-# Поле ввода 4 (window_length)
-entry5 = tk.Entry(root)
-entry5.insert(0, "25")
-entry5.bind("<FocusIn>", on_entry_click)
-entry5.pack()
-entry5.place(x=40, y=285)
-# Поле ввода 4 (polyorder)
-entry6 = tk.Entry(root)
-entry6.insert(0, "2")
-entry6.bind("<FocusIn>", on_entry_click)
-entry6.pack()
-entry6.place(x=40, y=305)
-#find_picks
-# Поле ввода 5 (width)
-entry7 = tk.Entry(root)
-entry7.insert(0, "10")
-entry7.bind("<FocusIn>", on_entry_click)
-entry7.pack()
-entry7.place(x=40, y=385)
-# Поле ввода 6 (prominence)
-entry8 = tk.Entry(root)
-entry8.insert(0, "10")
-entry8.bind("<FocusIn>", on_entry_click)
-entry8.pack()
-entry8.place(x=40, y=405)
-
-
-# Поле инструментов
-mainmenu.add_cascade(label="Файл", menu=filemenu)
-mainmenu.add_cascade(label="Справка", menu=helpmenu)
-
-mainmenu.add_cascade(label="Действия над файлом", menu=building)
-mainmenu.add_command(label="run", command=get_input)
-
-# Создаем метку (Label) с текстом
-label = tk.Label(root, text="Функции и параметры", font=("Arial", 13))
-label.pack()  # Размещаем метку на окне
-label.place(x=5, y=5)
-
-
-label = tk.Label(root, text="Удаление БЛ", font=("Arial", 11))
-label.pack()  # Размещаем метку на окне
-label.place(x=5, y=95)
-
-label = tk.Label(root, text="Нормировка", font=("Arial", 11))
-label.pack()  # Размещаем метку на окне
-label.place(x=5, y=180)
-label = tk.Label(root, text="Сглаживание", font=("Arial", 11))
-label.pack()  # Размещаем метку на окне
-label.place(x=5, y=242)
-
-
-checkbox_var = tk.IntVar()
-checkbox = tk.Checkbutton(root, text="Метод BL_ALS", font=("Arial", 10), variable=checkbox_var, command=actions1)
-checkbox.pack()
-checkbox.place(x=5, y=115)
-
-checkbox_var2 = tk.IntVar()
-checkbox2 = tk.Checkbutton(root, text="Поиск пиков", variable=checkbox_var2, command=actions3)
-checkbox2.pack()
-checkbox2.place(x=5, y=360)
-
-checkbox_var4 = tk.IntVar()
-checkbox4 = tk.Checkbutton(root, text="Норм. методом SNV", variable=checkbox_var4, command=actions5)
-checkbox4.pack()
-checkbox4.place(x=5, y=200)
-
-checkbox_var3 = tk.IntVar()
-checkbox3 = tk.Checkbutton(root, text="Норм. по макс", variable=checkbox_var3, command=actions4)
-checkbox3.pack()
-checkbox3.place(x=5, y=220)
-
-checkbox_var6 = tk.IntVar()
-checkbox6 = tk.Checkbutton(root, text="Фильт. Савицкого-Голая", variable=checkbox_var6, command=actions6)
-checkbox6.pack()
-checkbox6.place(x=5, y=260)
-
-# checkbox_var8 = tk.IntVar()
-# checkbox8 = tk.Checkbutton(root, text="Метод ср. скользящей", variable=checkbox_var8, command=actions7)
-# checkbox8.pack()
-# checkbox8.place(x=5, y=322)
-
-checkbox_var7 = tk.IntVar()
-checkbox7 = tk.Checkbutton(root, text="Нахожение среднего", variable=checkbox_var7, command=actions2)
-checkbox7.pack()
-checkbox7.place(x=5, y=342)
-
-checkbox_var8 = tk.IntVar()
-checkbox8 = tk.Checkbutton(root, text="Выбор полосы частот", variable=checkbox_var8, command=actions9)
-checkbox8.pack()
-checkbox8.place(x=5, y=25)
-
-# Создаем фрейм для размещения downbar
-bottom_frame = tk.Frame(root, height=30, bg='lightgray')
-bottom_frame.pack(side='bottom', fill='x')
-
-# Отображаем версию tkinter на полосе
-Program_version = 0.3572
-version_label = tk.Label(bottom_frame, text=f"Program version: {Program_version}", bg='lightgray')
-version_label.pack(side='right', padx=10)
-
-
-root.mainloop()
+if __name__ == '__main__':
+    app.run(debug=True)
