fe5b30c397db108cbb05172a883025dfce3eec33
nstk24 <nastkinsergej24@gmail.com>
2024-12-23 17:42:57 +0400

    q

--- app.py
+++ app.py
@@ -439,5 +439,7 @@ def index():
     return render_template('index.html')
 
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
